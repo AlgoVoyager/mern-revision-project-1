f0f91bd74db23c0df40e4df497eb8af8235b6d06
AlgoVoyager <nishantdewangan2002@gmail.com>
2026-09-03 06:20:54 +0530

    feat: localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,13 @@ import Navbar from './components/ui/Navbar'
 import Dashboard from './components/Dashboard'
 import TaskForm from './components/tasks/TaskForm'
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        title: "Learn React",
-        status: "In Progress",
-        priority: "Medium",
-        createdAt:"2026-09-03T00:27:51.166Z"
-      },
-      {
-        id: 2,
-        title: "Build MERN Project",
-        status: "Todo",
-        priority: "High",
-        createdAt:"2026-09-03T00:28:20.874Z"
-      },
-      {
-        id: 3,
-        title: "Practice DSA",
-        status: "Done",
-        priority: "Low",
-        createdAt:"2026-09-03T00:28:24.536Z"
-    }
-  ])
+  const [tasks, setTasks] = useState(()=>{
+    const storedTasks = localStorage.getItem("tasks");
+    return storedTasks?JSON.parse(storedTasks):[];
+  })
+  useEffect(()=>{
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+  },[tasks])
   const statuses = ["Todo", "In Progress", "Done"];
   const onStatusChange = (id) => {
     const task = tasks.find(t=>t.id==id);
--- src/utils/data.js
+++ src/utils/data.js
@@ -3,21 +3,24 @@ const tasks = [
         id: 1,
         title: "Learn React",
         status: "In Progress",
-        priority: "High"
-    },
-    {
+        priority: "Medium",
+        createdAt:"2026-09-03T00:27:51.166Z"
+      },
+      {
         id: 2,
         title: "Build MERN Project",
         status: "Todo",
-        priority: "Medium"
-    },
-    {
+        priority: "High",
+        createdAt:"2026-09-03T00:28:20.874Z"
+      },
+      {
         id: 3,
         title: "Practice DSA",
         status: "Done",
-        priority: "Low"
+        priority: "Low",
+        createdAt:"2026-09-03T00:28:24.536Z"
     }
-];
+  ]
 
 export  {
     tasks
