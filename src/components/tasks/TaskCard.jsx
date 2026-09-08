6d163067879b14f2f97de71d96e4c9fb1d32863c
AlgoVoyager <nishantdewangan2002@gmail.com>
2026-09-08 07:53:06 +0530

    taskcard  fix  / server init

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -40,7 +40,7 @@ const TaskCard = ({task, onEditingTask}) => {
         <h3 className='py-1  text-white'>{task.title}</h3>
         <div className={`task-status text-sm rounded-md p-1 w-fit cursor-pointer ${task_status_color} `} onClick={()=>onStatusChange(task.id)} title='Click to change'>{task.status}</div>
         <div className={"task-priority text-sm rounded-md p-1 w-fit "+priority_color}>{task.priority} Priority</div>
-        <DisplayDate dateval={task.dueDate} mode={"due"} />
+        {task.status !== "Done" && <DisplayDate mode="due" dateval={task.dueDate} />}
         <button onClick={()=>{onEditingTask(task)}}>✏️</button>
         <button onClick={()=>{onDeleteTask(task.id)}}>🗑️</button>
     </div>
